7557fbe7f89117ab93fac60093f307f5151e268c
sabaruto <theodoreaaronobelley@hotmail.co.uk>
2022-07-31 21:30:09 +0100

    2 files modified, 8 added, 1 deleted

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,9 @@
 import { Product } from './products';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class CartService {
   items: Product[] = [];
 
