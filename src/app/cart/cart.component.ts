2cd7b685ec7b9509da2e40b876ede7aee865e1f6
sabaruto <theodoreaaronobelley@hotmail.co.uk>
2022-08-01 20:29:36 +0100

    4 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Product } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {}
+export class CartComponent {
+  items = this.cartService.getItems();
+  constructor(private cartService: CartService) {}
+}
